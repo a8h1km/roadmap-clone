e756d55350cfd45b5f0c50292a204520af827098
a8h1km <mightyincognito07@gmail.com>
2023-12-09 12:35:31 +0530

    -a

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    mode: 'jit',
+    purge: [],
+    darkMode: false,
+    theme: {
+        fontWeight: {
+          thin: '100',
+          hairline: '100',
+          extralight: '200',
+          light: '300',
+          normal: '4500',
+          medium: '500',
+          semibold: '600',
+          bold: '700',
+          extrabold: '800',
+          'extra-bold': '800',
+          black: '900',
+        }
+        
+      }
+}
